6e6dce78dd6d221ba06ed21b3ad9cc1795118f24
quapka <quapka@gmail.com>
2025-10-15 17:23:50 +0200

    Outsource curve parameters to JCMathLib

--- applet/src/main/java/applet/IndistinguishabilityApplet.java
+++ applet/src/main/java/applet/IndistinguishabilityApplet.java
@@ -26,6 +26,7 @@ import applet.Consts;
 
 import applet.jcmathlib.OperationSupport;
 import applet.jcmathlib.ResourceManager;
+import applet.jcmathlib.SecP256r1;
 
 // FIXME change all (short) 0 to ZERO final 0x00 byte value?
 public class IndistinguishabilityApplet extends Applet implements ExtendedLength
@@ -401,8 +402,8 @@ public class IndistinguishabilityApplet extends Applet implements ExtendedLength
         OIDC_PUBLIC_KEY.setA(SecP256r1.a, offset, (short) SecP256r1.a.length);
         OIDC_PUBLIC_KEY.setB(SecP256r1.b, offset, (short) SecP256r1.b.length);
         OIDC_PUBLIC_KEY.setG(SecP256r1.G, offset, (short) SecP256r1.G.length);
-        OIDC_PUBLIC_KEY.setR(SecP256r1.n, offset, (short) SecP256r1.n.length);
-        OIDC_PUBLIC_KEY.setK(SecP256r1.h);
+        OIDC_PUBLIC_KEY.setR(SecP256r1.r, offset, (short) SecP256r1.r.length);
+        OIDC_PUBLIC_KEY.setK(SecP256r1.k);
         OIDC_PUBLIC_KEY.setW(buffer, (short) ISO7816.OFFSET_CDATA, (short) uncompressPubKeySize);
 
         getOIDCPublicKey(apdu);
@@ -459,15 +460,15 @@ public class IndistinguishabilityApplet extends Applet implements ExtendedLength
         pubDVRFKey.setA(SecP256r1.a, offset, (short) SecP256r1.a.length);
         pubDVRFKey.setB(SecP256r1.b, offset, (short) SecP256r1.b.length);
         pubDVRFKey.setG(SecP256r1.G, offset, (short) SecP256r1.G.length);
-        pubDVRFKey.setR(SecP256r1.n, offset, (short) SecP256r1.n.length);
-        pubDVRFKey.setK(SecP256r1.h);
+        pubDVRFKey.setR(SecP256r1.r, offset, (short) SecP256r1.r.length);
+        pubDVRFKey.setK(SecP256r1.k);
 
         privDVRFKey.setFieldFP(SecP256r1.p, offset, (short) SecP256r1.p.length);
         privDVRFKey.setA(SecP256r1.a, offset, (short) SecP256r1.a.length);
         privDVRFKey.setB(SecP256r1.b, offset, (short) SecP256r1.b.length);
         privDVRFKey.setG(SecP256r1.G, offset, (short) SecP256r1.G.length);
-        privDVRFKey.setR(SecP256r1.n, offset, (short) SecP256r1.n.length);
-        privDVRFKey.setK(SecP256r1.h);
+        privDVRFKey.setR(SecP256r1.r, offset, (short) SecP256r1.r.length);
+        privDVRFKey.setK(SecP256r1.k);
 
         ecKeyPair = new KeyPair(pubDVRFKey, privDVRFKey);
 
@@ -631,75 +632,6 @@ public class IndistinguishabilityApplet extends Applet implements ExtendedLength
     private void base64UrlsafeDecode(byte[] encoded, short length) {
         // 
     }
-
-    // FIXME do not duplicate the curve values to what is already in JCMathLib
-    public static class SecP256r1 {
-        public final static byte[] p = {
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
-            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x01,
-            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff
-        };
-        public final static byte[] a = {
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
-            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x01,
-            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xfc
-        };
-        public final static byte[] b = {
-            (byte) 0x5a, (byte) 0xc6, (byte) 0x35, (byte) 0xd8,
-            (byte) 0xaa, (byte) 0x3a, (byte) 0x93, (byte) 0xe7,
-            (byte) 0xb3, (byte) 0xeb, (byte) 0xbd, (byte) 0x55,
-            (byte) 0x76, (byte) 0x98, (byte) 0x86, (byte) 0xbc,
-            (byte) 0x65, (byte) 0x1d, (byte) 0x06, (byte) 0xb0,
-            (byte) 0xcc, (byte) 0x53, (byte) 0xb0, (byte) 0xf6,
-            (byte) 0x3b, (byte) 0xce, (byte) 0x3c, (byte) 0x3e,
-            (byte) 0x27, (byte) 0xd2, (byte) 0x60, (byte) 0x4b
-        };
-        public final static byte[] G = {
-            (byte) 0x04,
-            // x-coordinate
-            (byte) 0x6b, (byte) 0x17, (byte) 0xd1, (byte) 0xf2,
-            (byte) 0xe1, (byte) 0x2c, (byte) 0x42, (byte) 0x47,
-            (byte) 0xf8, (byte) 0xbc, (byte) 0xe6, (byte) 0xe5,
-            (byte) 0x63, (byte) 0xa4, (byte) 0x40, (byte) 0xf2,
-            (byte) 0x77, (byte) 0x03, (byte) 0x7d, (byte) 0x81,
-            (byte) 0x2d, (byte) 0xeb, (byte) 0x33, (byte) 0xa0,
-            (byte) 0xf4, (byte) 0xa1, (byte) 0x39, (byte) 0x45,
-            (byte) 0xd8, (byte) 0x98, (byte) 0xc2, (byte) 0x96,
-            // y-coordinate
-            (byte) 0x4f, (byte) 0xe3, (byte) 0x42, (byte) 0xe2,
-            (byte) 0xfe, (byte) 0x1a, (byte) 0x7f, (byte) 0x9b,
-            (byte) 0x8e, (byte) 0xe7, (byte) 0xeb, (byte) 0x4a,
-            (byte) 0x7c, (byte) 0x0f, (byte) 0x9e, (byte) 0x16,
-            (byte) 0x2b, (byte) 0xce, (byte) 0x33, (byte) 0x57,
-            (byte) 0x6b, (byte) 0x31, (byte) 0x5e, (byte) 0xce,
-            (byte) 0xcb, (byte) 0xb6, (byte) 0x40, (byte) 0x68,
-            (byte) 0x37, (byte) 0xbf, (byte) 0x51, (byte) 0xf5
-        };
-        // the order of G
-        public final static byte[] n = {
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
-            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
-            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
-            (byte) 0xbc, (byte) 0xe6, (byte) 0xfa, (byte) 0xad,
-            (byte) 0xa7, (byte) 0x17, (byte) 0x9e, (byte) 0x84,
-            (byte) 0xf3, (byte) 0xb9, (byte) 0xca, (byte) 0xc2,
-            (byte) 0xfc, (byte) 0x63, (byte) 0x25, (byte) 0x51
-        };
-        // cofactor
-        public final static short h = 0x01;
-    }
-
 
     private static final byte PADDING = '=';
 
