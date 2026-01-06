cd7b0013d081eb07a65ad880a3626cec498b035e
quapka <quapka@gmail.com>
2026-01-06 18:11:58 +0100

    Define constants consistently

--- applet/src/main/java/applet/HashCustom.java
+++ applet/src/main/java/applet/HashCustom.java
@@ -14,25 +14,22 @@ public class HashCustom {
     };
 
     public static byte[] MUSIG_NONCECOEF = new byte[] {
-        (byte) 90, (byte) 109, (byte) 69, (byte) 246, (byte) 218, (byte) 41,
-            (byte) 230, (byte) 81, (byte) 203, (byte) 27, (byte) 162, (byte) 184, (byte) 172, (byte) 44, (byte) 221, (byte) 78,
-            (byte) 188, (byte) 21, (byte) 194, (byte) 251, (byte) 178, (byte) 137, (byte) 240, (byte) 204, (byte) 130, (byte) 27,
-            (byte) 191, (byte) 10, (byte) 52, (byte) 9, (byte) 95, (byte) 50
+        (byte) 0x5a, (byte) 0x6d, (byte) 0x45, (byte) 0xf6, (byte) 0xda, (byte) 0x29, (byte) 0xe6, (byte) 0x51,
+        (byte) 0xcb, (byte) 0x1b, (byte) 0xa2, (byte) 0xb8, (byte) 0xac, (byte) 0x2c, (byte) 0xdd, (byte) 0x4e,
+        (byte) 0xbc, (byte) 0x15, (byte) 0xc2, (byte) 0xfb, (byte) 0xb2, (byte) 0x89, (byte) 0xf0, (byte) 0xcc,
+        (byte) 0x82, (byte) 0x1b, (byte) 0xbf, (byte) 0x0a, (byte) 0x34, (byte) 0x09, (byte) 0x5f, (byte) 0x32
     };
 
-    public static byte[] BIP_CHALLENGE = new byte[] {(byte) 123, (byte) 181, (byte) 45, (byte) 122, (byte) 159, (byte) 239,
-            (byte) 88, (byte) 50, (byte) 62, (byte) 177, (byte) 191, (byte) 122, (byte) 64, (byte) 125, (byte) 179, (byte) 130,
-            (byte) 210, (byte) 243, (byte) 242, (byte) 216, (byte) 27, (byte) 177, (byte) 34, (byte) 79, (byte) 73, (byte) 254,
-            (byte) 81, (byte) 143, (byte) 109, (byte) 72, (byte) 211, (byte) 124};
+    public static byte[] BIP_CHALLENGE = new byte[] {
+        (byte) 0x7b, (byte) 0xb5, (byte) 0x2d, (byte) 0x7a, (byte) 0x9f, (byte) 0xef, (byte) 0x58, (byte) 0x32,
+        (byte) 0x3e, (byte) 0xb1, (byte) 0xbf, (byte) 0x7a, (byte) 0x40, (byte) 0x7d, (byte) 0xb3, (byte) 0x82,
+        (byte) 0xd2, (byte) 0xf3, (byte) 0xf2, (byte) 0xd8, (byte) 0x1b, (byte) 0xb1, (byte) 0x22, (byte) 0x4f,
+        (byte) 0x49, (byte) 0xfe, (byte) 0x51, (byte) 0x8f, (byte) 0x6d, (byte) 0x48, (byte) 0xd3, (byte) 0x7c
+    };
 
     // tagHash = SHA-256.update(b"Indistinguishability service")
     // SHA-256.update(tagHash).update(tagHash).digest()
     public static byte[] INDISTINGUISHABILITY_SERVICE = new byte[] {
-        // (byte) 0x1d, (byte) 0x00, (byte) 0x29, (byte) 0x96, (byte) 0xb5, (byte) 0x1d, (byte) 0xf5, (byte) 0xb2,
-        // (byte) 0xe3, (byte) 0x3e, (byte) 0x97, (byte) 0xdb, (byte) 0x23, (byte) 0xcc, (byte) 0xae, (byte) 0x50,
-        // (byte) 0x28, (byte) 0x54, (byte) 0x10, (byte) 0xdb, (byte) 0x82, (byte) 0x46, (byte) 0x49, (byte) 0xa5,
-        // (byte) 0x5f, (byte) 0x2b, (byte) 0x7d, (byte) 0xb7, (byte) 0xff, (byte) 0x94, (byte) 0x46, (byte) 0x68,
-
         (byte) 0xec, (byte) 0x29, (byte) 0xf4, (byte) 0x68, (byte) 0x2e, (byte) 0x2b, (byte) 0x14, (byte) 0xbf,
         (byte) 0xcd, (byte) 0xcf, (byte) 0xf9, (byte) 0xe1, (byte) 0x84, (byte) 0xf7, (byte) 0xf5, (byte) 0xe4,
         (byte) 0x22, (byte) 0x9b, (byte) 0x0c, (byte) 0x12, (byte) 0x05, (byte) 0x75, (byte) 0x01, (byte) 0xa8,
